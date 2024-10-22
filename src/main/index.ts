b408ea96d75137c1e7e5ee3bf8d4a39cfe00b769
Mike Ratcliffe <michael@ratcliffefamily.org>
2024-10-23 00:19:41 +0100

    build: add electron-debug

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,9 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import debug from 'electron-debug'
+
+debug()
 
 function createWindow(): void {
   // Create the browser window.
